80915c97fa5b92cb414671978ac02e1819bd8a7a
bonschorno <colin.walder@hotmail.com>
2021-01-28 13:15:52 +0100

    reverting to 8af3376

---
+++ doc/Walk-through.R
@@ -0,0 +1,15 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  message = FALSE,
+  warning = FALSE
+)
+
+## -----------------------------------------------------------------------------
+# install.packages("devtools") 
+#devtools::install_github("bonschorno/sep")
+
+## ----setup--------------------------------------------------------------------
+library(sep)
+

e77c8a53dbedc2deb865d5440e0685774e7f6b85
bonschorno <colin.walder@hotmail.com>
2021-04-19 16:19:47 +0200

    adding percentage helper function

--- R/helper_percentage.R
+++ R/helper_percentage.R
@@ -1,3 +1,12 @@
-helper_percentage <- function(x, digits=1) {
-  sprintf(paste0("%1.", digits, "f%%"), x*100)
+#' Helper function to easily calculate/display relative frequencies
+#'
+#' @param item a survey item
+#' @param digits number of digits to be displayed
+#'
+#' @return relative frequencies of a survey item
+#' @export
+#'
+#'
+helper_percentage <- function(item, digits=1) {
+  sprintf(paste0("%1.", digits, "f%%"), item*100)
 }

7c1f24f7fe3ee64cfaeecb0d8b3abfe89f4e2152
sassifer <104585034+sassifer@users.noreply.github.com>
2022-10-10 21:58:24 +0200

    Add files via upload

--- R/n_par.R
+++ R/n_par.R
@@ -2,6 +2,8 @@
 #'
 #' @param data a \code{data.frame} object
 #' @param item relevant survey item (i.e. w5_q17)
+#' @param by optional argument for distinction by subgroup
+#' @param treat optional argument for distinction by treatment group
 #' @param lang language (German = "DE" (default), English = "EN", anything else goes neutral)
 #'
 #' @return number of participants ("Don't knows"-answers included) for item
@@ -10,17 +12,76 @@
 #'
 #' @export
 #'
-n_par <- function(data, item, lang = "DE"){
 
-  n <- data %>%
-    select({{item}}) %>%
-    filter({{item}} >= -9) %>%
-    count() %>%
-    pull()
-
-  n_text <- case_when(lang == "DE" ~ "Grafik basiert auf N = ",
-                      lang == "EN" ~ "Plot is based on N = ",
-                      TRUE ~ "N = ")
-
-  participants <- paste0("\n", n_text, n)
+n_par <- function(data, item, by = NULL, treat = NULL, lang = "DE"){
+  if(is.null(by) & is.null(treat)){
+    count <- data %>% 
+      dplyr::select(all_of(item)) %>% 
+      pivot_longer(all_of(item), 
+                   names_to = "question", values_to = "value") %>% 
+      filter(value > -1 & !is.na(value)) %>% 
+      group_by(question) %>% 
+      count() %>% 
+      pull(n) %>% 
+      unique()
+  } else if(is.null(treat)){
+    count <- data %>% 
+      dplyr::select(all_of(item), by = all_of(by)) %>% 
+      pivot_longer(all_of(item), 
+                   names_to = "question", values_to = "value") %>% 
+      filter(value > -1 & !is.na(value)) %>% 
+      pivot_longer(by,
+                   names_to = "category", values_to = "subgroup") %>% 
+      filter(!is.na(subgroup)) %>% 
+      group_by(question, category, subgroup) %>% 
+      count() %>% 
+      pull(n) %>% 
+      unique()
+  } else if(is.null(by)){
+    count <- data %>% 
+      dplyr::select(all_of(item), treat = all_of(treat)) %>% 
+      pivot_longer(all_of(item), 
+                   names_to = "question", values_to = "value") %>% 
+      filter(value > -1 & !is.na(value)) %>% 
+      pivot_longer(treat,
+                   names_to = "treatment", values_to = "group") %>% 
+      filter(!is.na(group)) %>% 
+      group_by(question, treatment, group) %>% 
+      count() %>% 
+      pull(n) %>% 
+      unique()
+  } else {
+    count <- data %>% 
+      dplyr::select(all_of(item), by = all_of(by), treat = all_of(treat)) %>% 
+      pivot_longer(all_of(item), 
+                   names_to = "question", values_to = "value") %>% 
+      filter(value > -1 & !is.na(value)) %>% 
+      pivot_longer(treat,
+                   names_to = "treatment", values_to = "group") %>% 
+      filter(!is.na(group)) %>% 
+      pivot_longer(by,
+                   names_to = "category", values_to = "subgroup") %>% 
+      filter(!is.na(subgroup)) %>% 
+      group_by(question, treatment, group, category, subgroup) %>% 
+      count() %>% 
+      pull(n) %>% 
+      unique()
+  }
+  
+  if(lang == "DE"){
+    if(length(count) == 1 & length(item) == 1){paste("Grafik basiert auf N = ", count, sep = "")} 
+    else if(length(count) == 1 & is.null(by) & is.null(treat)){paste("Grafik basiert auf N = ", count, " (pro Unterfrage).", sep = "")}
+    else if(length(count) == 1){paste("Grafik basiert auf N = ", count, " (pro Unterfrage und Untergruppe).", sep = "")}
+    else if(is.null(by) & is.null(treat)){paste("Die Anzahl Befragter variiert je nach Unterfrage zwischen ", min(count), " und ", max(count), ".", sep = "")}
+    else {paste("Die Anzahl Befragter variiert je nach Unterfrage und Untergruppe zwischen ", min(count), " und ", max(count), ".", sep = "")}
+  } else if(lang == "EN"){
+    if(length(count) == 1 & length(item) == 1){paste("Plot is based on N = ", count, sep = "")} 
+    else if(length(count) == 1 & is.null(by) & is.null(treat)){paste("Plot is based on N = ", count, " (per subitem).", sep = "")}
+    else if(length(count) == 1){paste("Plot is based on N = ", count, " (per subgroup and subitem).", sep = "")}
+    else if(is.null(by) & is.null(treat)){paste("The number of participants varies by subitem between ", min(count), " and ", max(count), ".", sep = "")}
+    else {paste("The number of participants varies by subitem and subgroup between ", min(count), " and ", max(count), ".", sep = "")}
+  } else {
+    paste("N =", max(count))
+  }
+  
 }
--- R/plot_rank_v.R
+++ R/plot_rank_v.R
@@ -1,29 +1,29 @@
-#' Horizontal barplot for multiple items
+#' Vertical barplot to rank items
 #'
 #' @param data a \code{data.frame} object
 #' @param item a survey item
 #' @param by optional argument for distinction by subgroup
 #' @param treat optional argument for distinction by treatment group
 #' @param lang optional argument for language (German = "DE" (default), English = "EN")
-#' @param barwidth optional argument to define the width of the bar
+#' @param n.items optional argument to set the number of items per rank (default: all items; if fewer: items with highest values)
 #' @param ncol.wrap optional argument to set the number of facet_wrap columns when distinguishing by subgroup or treatment group
 #' @param textsize optional argument to adjust the text's size
 #' @param min_textsize optional argument to set the minimum text size
 #' @param percent_position optional argument to set position of percentage within bar ("center", "left", "top", etc.)
+#' @param barwidth optional argument to define the width of the bar
 #' @param legend.pos optional argument to set position of legend
 #'
-#' @return a vertical barplot for ranking
+#' @return a horizontal barplot for multiple items
 #' @export
 #'
 #' @import tidyverse
 #' @import ggfittext
 #'
-#'
 
-plot_multiple_h <- function(data, item, by = NULL, treat = NULL, lang = "DE",
-                            barwidth = 0.6, ncol.wrap = 1, 
-                            textsize = 8, min_textsize = 5, percent_position = "center",
-                            legend.pos = "bottom"){
+plot_rank_v <- function(data, item, by = NULL, treat = NULL, lang = "DE",
+                        n.items = length(labs), ncol.wrap = 1,
+                        textsize = 8, min_textsize = 5, percent_position = "center",
+                        barwidth = 0.6, legend.pos = "right"){
   
   # error message if question or labels are not predefined
   environment <- ls(.GlobalEnv)
@@ -41,7 +41,11 @@ plot_multiple_h <- function(data, item, by = NULL, treat = NULL, lang = "DE",
       group_by(variable) %>% 
       mutate(total_item = sum(n),
              freq_rel = n/total_item,
-             percentage = paste0(round(freq_rel*100, 1), "%"))
+             percentage = paste0(round(freq_rel*100, 1), "%"),
+             rank = rank(desc(freq_rel))) %>% 
+      ungroup() %>% 
+      filter(rank <= n.items)
+    
   } else if(!is.null(treat) & is.null(by)) { #item with treatment groups
     plot <- data %>% 
       dplyr::select(all_of(item), treat = all_of(treat)) %>% 
@@ -52,7 +56,10 @@ plot_multiple_h <- function(data, item, by = NULL, treat = NULL, lang = "DE",
       group_by(treat, variable) %>%
       mutate(total_item = sum(n),
              freq_rel = n/total_item,
-             percentage = paste0(round(freq_rel*100, 1), "%"))
+             percentage = paste0(round(freq_rel*100, 1), "%"),
+             rank = rank(desc(freq_rel))) %>% 
+      ungroup() %>% 
+      filter(rank <= n.items)
   } else if(is.null(treat) & !is.null(by)) { #item with subgroups
     plot <- data %>% 
       dplyr::select(all_of(item), by = all_of(by)) %>% 
@@ -63,7 +70,10 @@ plot_multiple_h <- function(data, item, by = NULL, treat = NULL, lang = "DE",
       group_by(by, variable) %>%
       mutate(total_item = sum(n),
              freq_rel = n/total_item,
-             percentage = paste0(round(freq_rel*100, 1), "%"))
+             percentage = paste0(round(freq_rel*100, 1), "%"),
+             rank = rank(desc(freq_rel))) %>% 
+      ungroup() %>% 
+      filter(rank <= n.items)
   } else { #item with both treatment and subgroups
     plot <- data %>% 
       dplyr::select(all_of(item), treat = all_of(treat), by = all_of(by)) %>% 
@@ -74,12 +84,26 @@ plot_multiple_h <- function(data, item, by = NULL, treat = NULL, lang = "DE",
       group_by(treat, by, variable) %>%
       mutate(total_item = sum(n),
              freq_rel = n/total_item,
-             percentage = paste0(round(freq_rel*100, 1), "%"))
+             percentage = paste0(round(freq_rel*100, 1), "%"),
+             rank = rank(desc(freq_rel))) %>% 
+      ungroup() %>% 
+      filter(rank <= n.items) 
+  }
+  
+  # give proper rank labels
+  if(lang == "DE"){ #German
+    for(i in 1:length(item)){
+      plot$variable <- ifelse(plot$variable == item[i], paste("Rang\n", i), plot$variable)
+    }
+  } else { # English
+    for(i in 1:length(item)){
+      plot$variable <- ifelse(plot$variable == item[i], paste("Rank\n", i), plot$variable)
+    }
   }
   
   # give proper labels (item text)
-  for(i in 1:length(item)){ 
-    plot$variable <- ifelse(plot$variable == item[i], labs[i], plot$variable)
+  for(i in unique(plot$value)){ 
+    plot$value <- ifelse(plot$value == i, labs[i], plot$value)
   }
   
   # set caption according to language
@@ -93,7 +117,7 @@ plot_multiple_h <- function(data, item, by = NULL, treat = NULL, lang = "DE",
                           sep = ""))
   
   # plot
-  p <- ggplot(plot, aes(freq_rel, factor(variable, levels = rev(labs)),
+  p <- ggplot(plot, aes(factor(variable, levels = unique(variable)), freq_rel,
                         fill = factor(value), label = percentage)) +
     geom_col(width = barwidth) +
     ggfittext::geom_bar_text(outside = FALSE,
@@ -101,18 +125,18 @@ plot_multiple_h <- function(data, item, by = NULL, treat = NULL, lang = "DE",
                              place = percent_position,
                              size = textsize, 
                              min.size = min_textsize) +
-    scale_x_continuous(labels = scales::percent) +
     labs(x = NULL, y = NULL, caption = caption) +
     irpanels::theme_sep() +
+    scale_y_continuous(labels = scales::percent) +
     theme(plot.caption = element_text(color = "grey"),
           plot.caption.position =  "plot",
           plot.title = element_text(hjust = 0.5),
           legend.position = legend.pos,
-          panel.grid.major.y = element_blank(),
-          panel.grid.major.x = element_line(linetype = "dashed"),
-          panel.grid.minor.x = element_line(linetype = "dashed")) +
+          panel.grid.major.x = element_blank(),
+          panel.grid.major.y = element_line(linetype = "dashed"),
+          panel.grid.minor.y = element_line(linetype = "dashed")) +
     guides(fill = guide_legend(keywidth = 0.4, keyheight = 0.8, 
-                               label = T, reverse = T, title = NULL))
+                               label = T, reverse = F, title = NULL))
   
   # print plot
   if(is.null(treat) & is.null(by)) { #item without treatment or subgroups
@@ -122,6 +146,6 @@ plot_multiple_h <- function(data, item, by = NULL, treat = NULL, lang = "DE",
   } else if(is.null(treat) & !is.null(by)) { #item with subgroups
     p + facet_wrap(~ by, ncol = ncol.wrap)
   } else { #item with both treatment and subgroups
-    p + facet_grid(treat ~ by)
+    p +  facet_grid(treat ~ by)
   }
 }

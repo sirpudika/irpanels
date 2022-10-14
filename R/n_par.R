921703aa38995456d8b5efea8900ae7609b545cd
sassifer <104585034+sassifer@users.noreply.github.com>
2022-10-14 16:33:26 +0200

    Update n_par.R

--- R/n_par.R
+++ R/n_par.R
@@ -18,7 +18,7 @@ n_par <- function(data, item, by = NULL, treat = NULL, lang = "DE"){
   # rename treatment and subgroup variables
  # data$by <- data[, quote(by)]
   #data$treat <- data[, quote(treat)]
-  if(length({{item}}) == 1){data$item <- data[, {{item}}]}
+  if(length(eval(quote(item))) == 1){data$item <- data[, eval(quote(item))]}
   
   # count
   if(is.null(by) & is.null(treat)){ # without treatment or subgroup

a7d2c79ed8f306257a98c1c868b5af7442c7145e
sassifer <104585034+sassifer@users.noreply.github.com>
2022-10-14 14:09:07 +0200

    Update plot_multiple_h.R

--- R/plot_multiple_h.R
+++ R/plot_multiple_h.R
@@ -31,7 +31,7 @@ plot_multiple_h <- function(data, item, by = NULL, treat = NULL, lang = "DE",
             "Question text ('question') must be defined." = ("question" %in% environment))
   
   # compute numbers
-  if(is.null(treat) & is.null(by)) { #item without treatment or subgroups
+  if(is.null({{treat}}) & is.null({{by}})) { #item without treatment or subgroups
     plot <- data %>% 
       dplyr:: select(all_of(item)) %>% 
       pivot_longer(all_of(item), names_to = "variable", values_to = "value") %>%
@@ -42,7 +42,7 @@ plot_multiple_h <- function(data, item, by = NULL, treat = NULL, lang = "DE",
       mutate(total_item = sum(n),
              freq_rel = n/total_item,
              percentage = paste0(round(freq_rel*100, 1), "%"))
-  } else if(!is.null(treat) & is.null(by)) { #item with treatment groups
+  } else if(!is.null({{treat}}) & is.null({{by}})) { #item with treatment groups
     plot <- data %>% 
       dplyr::select(all_of(item), treat = {{treat}}) %>% 
       pivot_longer(all_of(item), names_to = "variable", values_to = "value") %>% 
@@ -53,7 +53,7 @@ plot_multiple_h <- function(data, item, by = NULL, treat = NULL, lang = "DE",
       mutate(total_item = sum(n),
              freq_rel = n/total_item,
              percentage = paste0(round(freq_rel*100, 1), "%"))
-  } else if(is.null(treat) & !is.null(by)) { #item with subgroups
+  } else if(is.null({{treat}}) & !is.null({{by}})) { #item with subgroups
     plot <- data %>% 
       dplyr::select(all_of(item), by = {{by}}) %>% 
       pivot_longer(all_of(item), names_to = "variable", values_to = "value") %>% 
